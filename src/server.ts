1ac900e40732fcfea204580798ea3c7631c792df
Ricardo Higino <79026025+RicardoHigino@users.noreply.github.com>
2021-06-22 21:37:58 -0300

    att

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,6 @@
 import express from "express";
 
-// @types/express
 const app = express();
-
-app.get("/test", (request, response) => {
-
-    return response.send("Olá NLW")
-});
-
-app.post("/test-post", (request, response) => {
-    return response.send("Olá NLW método Post")
-})
 
 // http://localhost:3000
 app.listen(3000, () => console.log("Server is running"))
